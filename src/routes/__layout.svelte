78acb8f917cb23d6bf389ffa9c71cb293d2ad1ba
spences10 <spences10apps@gmail.com>
2021-09-15 18:53:48 +0100

    lau

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+
+
+<script>
+  import Nav from '$lib/nav.svelte'
+  import '../app.postcss'
+
+  export let pages
+</script>
+
+<Nav {pages} />
+
+<main class="container max-w-xl mx-auto px-4">
+  <slot />
+</main>
